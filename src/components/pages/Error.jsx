7f11f7bdc390fadc33b69b901e887c7eba32e86b
Angelina Stalnaya <angelinastalnaya.feb@gmail.com>
2024-10-16 17:32:43 +0300

    error page styled

--- src/components/pages/Error.jsx
+++ src/components/pages/Error.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { Typography, SvgIcon } from "@mui/material";
 import ComonBtn from "../buttons/ComonBtn";
 import { useNavigate } from "react-router-dom";
+import { CancelIcon } from "../../images/svg/SvgIcons";
 
 const Error = () => {
   const navigate = useNavigate();
 
   const handleBackBtnClick = () => navigate(-1);
   return (
-    <div className='error_page'>
-      Hmmmm.... that page does not exist.
+    <div className="error_page">
+      <SvgIcon
+        component={CancelIcon}
+        inheritViewBox
+        sx={{ width: "7em", height: "7em" }}
+      />
+      <Typography sx={{ fontSize: "30px" }}>
+        Hmmmm.... that page does not exist.
+      </Typography>
       <ComonBtn handleBtnClick={handleBackBtnClick}>Go back</ComonBtn>
     </div>
   );
